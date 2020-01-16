ec938c6196799117f1b6c36c2b6a2280ab361b5f
Estevão S. Vargas <grodolfs@gmail.com>
2020-01-16 11:25:30 -0300

    Update main.js

--- alpha/js/main.js
+++ alpha/js/main.js
@@ -1,3 +1,21 @@
+const firebase = require("firebase");
+// Required for side-effects
+require("firebase/firestore");
+
+firebase.initializeApp({
+    apiKey: "AIzaSyBv_v3wdS2FD9GLZN_en05XH4t4nclIK58",
+    authDomain: "pc-appstore.firebaseapp.com",
+    projectId: "pc-appstore",
+});
+
+var db = firebase.firestore();
+
+db.collection("users").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+    });
+});
+
 var str = window.location.href;
 
 function getCookie(name) {
